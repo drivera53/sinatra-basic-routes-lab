2be8e251003fae86009f373e78de8aba6c533248
Danny Rivera <samboard@hotmail.com>
2021-03-11 19:43:16 -0500

    commit tree e811f24e8e35f57c06d75de082f68965a6dcb6c8

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+    
+    get '/name' do
+        "My name is Dan"
+    end
+
+    get '/hometown' do
+        "My hometown is Sucua"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is bohemian rhapsody"
+    end
+
 end
